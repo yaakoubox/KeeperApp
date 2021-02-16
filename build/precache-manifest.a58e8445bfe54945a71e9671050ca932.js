8fe673c22c5d4b50fe38975c65bf171e81815bae
yaakoubox <yakoubhedjerci@gmail.com>
2021-02-16 12:23:42 +0100

    second commit

---
+++ build/precache-manifest.a58e8445bfe54945a71e9671050ca932.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f55a0e5c8f1866ba9da8",
+    "url": "/KeeperApp/static/js/main.5f49b12e.chunk.js"
+  },
+  {
+    "revision": "1c99d557396180b147e1",
+    "url": "/KeeperApp/static/js/runtime~main.0cc059cd.js"
+  },
+  {
+    "revision": "e8f193427a413e1bb575",
+    "url": "/KeeperApp/static/js/2.0cf37038.chunk.js"
+  },
+  {
+    "revision": "d71a9330db5aef022f51e91fee1abcd1",
+    "url": "/KeeperApp/index.html"
+  }
+];
